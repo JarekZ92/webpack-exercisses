b49fedd6a64c14cb2cc2a91c0334bb7abb47016e
Jarek <z.jarek92@gmail.com>
2018-08-22 10:10:01 +0200

    created a.js and b.js files

---
+++ a.js
@@ -0,0 +1,2 @@
+import add from 'b'
+import randomFunc from 'b'
---
+++ b.js
@@ -0,0 +1,8 @@
+export function add(a, b) {
+    return a + b
+}
+add(2, 2)
+
+export function randomFunc() {
+    console.log('Random text')
+}
